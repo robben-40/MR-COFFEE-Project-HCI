e5babd01c8abff2d5814e0d98dd7c3cfd235af3b
robben-40 <robbenathlee@gmail.com>
2026-02-26 00:32:46 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 document.querySelectorAll('.order-now').forEach(button => {
     button.addEventListener('click', function() {
         // Redirect to a specific page
-        window.location.href = '../html/menu.html'; // Replace with your desired URL
+        window.location.href = '../html/Menu.html'; // Replace with your desired URL
     });
+
 });
